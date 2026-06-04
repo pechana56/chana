90112deb3275519605acd6b53f007a692f187ad4
pechana56 <chanaamm6@gmail.com>
2026-06-04 11:35:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ elif st.button("การแปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
 elif st.button("การวิเคราะห์ข้อมูลเชิงสำรวจ"):
     st.switch_page("pages/EDA_app(1).py")
-    elif st.button("พยากรณ์ยอดขาย"):
+elif st.button("พยากรณ์ยอดขาย"):
     st.switch_page("pages/sale_predict.py")
